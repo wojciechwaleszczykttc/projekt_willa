345f08dd0452afb5262282fa2800395b50d02e8b
Wojciech Waleszczyk <32631733+wiru123321@users.noreply.github.com>
2023-11-15 19:53:16 +0100

    Add files via upload

---
+++ src/api/page/middlewares/page-populate-middleware.js
@@ -0,0 +1,85 @@
+"use strict";
+
+/**
+ * `page-populate-middleware` middleware
+ */
+
+const populate = {
+  contentSections: {
+    populate: {
+      picture: {
+        fields: ["url", "alternativeText", "caption", "width", "height"],
+      },
+      pictures: {
+        populate: true,
+      },
+      pictures: {
+        populate: {
+          fields: ["picture"],
+          picture: {
+            populate: true,
+          },
+        },
+      },
+      buttons: {
+        populate: true,
+      },
+      button: {
+        populate: true,
+      },
+      contactElement: {
+        populate: {
+          fields: ["contactElement"],
+        },
+      },
+      gridElement: {
+        populate: {
+          fields: ["gridElement"],
+          button: {
+            populate: true,
+          },
+        },
+      },
+      feature: {
+        populate: {
+          fields: ["title", "description", "showLink", "newTab", "url", "text"],
+          media: {
+            fields: ["url", "alternativeText", "caption", "width", "height"],
+          },
+        },
+      },
+      testimonials: {
+        populate: {
+          picture: {
+            fields: ["url", "alternativeText", "caption", "width", "height"],
+          },
+        },
+      },
+      plans: {
+        populate: ["product_features"],
+      },
+      submitButton: {
+        populate: true,
+      },
+    },
+  },
+  seo: {
+    fields: ["metaTitle", "metaDescription"],
+    populate: { shareImage: true },
+  },
+};
+
+module.exports = (config, { strapi }) => {
+  // Add your own logic here.
+  return async (ctx, next) => {
+    ctx.query = {
+      populate,
+      filters: { slug: ctx.query.filters.slug },
+      locale: ctx.query.locale,
+    };
+
+    console.log("page-populate-middleware.js: ctx.query = ", ctx.query);
+
+    await next();
+  };
+};
